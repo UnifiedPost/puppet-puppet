717b5943f3856f0680ea6e33b1063e9f0888f496
Kris Buytaert <sdog@inuits.be>
2012-06-01 13:13:57 +0200

    Adding sslverify support to the yumrepo type .. needs to be removed as it is supported in future puppet versions

--- manifests/install.pp
+++ manifests/install.pp
@@ -4,4 +4,11 @@
 #
 class puppet::install {
   package { 'puppet': ensure => installed }
+  file { '/usr/lib/ruby/site_ruby/1.8/puppet/type/yumrepo.rb':
+    source  => 'puppet:///modules/puppet/yumrepo.rb',
+    group   => '0',
+    owner   => '0',
+    mode    => '0644',
+  }
+
 }
