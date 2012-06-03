6f13606b1981117312e0780e658a0cf9e2af365d
Kris Buytaert <sdog@inuits.be>
2012-06-03 21:01:41 +0200

    No more ugly hack to distribute patched yumrepo.rb

--- manifests/install.pp
+++ manifests/install.pp
@@ -4,11 +4,5 @@
 #
 class puppet::install {
   package { 'puppet': ensure => installed }
-  file { '/usr/lib/ruby/site_ruby/1.8/puppet/type/yumrepo.rb':
-    source  => 'puppet:///modules/puppet/yumrepo.rb',
-    group   => '0',
-    owner   => '0',
-    mode    => '0644',
-  }
 
 }
